2baaa94098a2d2b95c742a9890b90f8b0a35ecf6
ypsitau <ypsitau@nifty.com>
2025-04-11 12:39:22 +0900

    updated an article

--- samples/cmdedit-display-test/cmdedit-display-test.cpp
+++ samples/cmdedit-display-test/cmdedit-display-test.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "jxglib/ST7789.h"
-#include "jxglib/Font/shinonome16.h"
+#include "jxglib/USBHost.h"
+#include "jxglib/SSD1306.h"
+#include "jxglib/Font/shinonome12.h"
 
 using namespace jxglib;
 
@@ -10,27 +11,14 @@ Display::Terminal terminal;
 int main()
 {
     ::stdio_init_all();
-    GPIO::KeyboardMatrix keyboard;
-    const Keyboard::KeySet keySetTbl[] = {
-        VK_1,    VK_2, VK_3,     VK_BACK,
-        VK_4,    VK_5, VK_6,     VK_UP,
-        VK_7,    VK_8, VK_9,     VK_DOWN,
-        VK_LEFT, VK_0, VK_RIGHT, VK_RETURN,
-    };
-    const GPIO::KeyRow keyRowTbl[] = { GPIO16, GPIO17, GPIO18, GPIO19 };
-    const GPIO::KeyCol keyColTbl[] = {
-        GPIO20.pull_up(), GPIO21.pull_up(), GPIO26.pull_up(), GPIO27.pull_up()
-    };
-    keyboard.Initialize(keySetTbl,
-        keyRowTbl, count_of(keyRowTbl),
-        keyColTbl, count_of(keyColTbl), GPIO::LogicNeg);
-    ::spi_init(spi1, 125 * 1000 * 1000);
-    GPIO14.set_function_SPI1_SCK();
-    GPIO15.set_function_SPI1_TX();
-    ST7789 display(spi1, 240, 320, {RST: GPIO10, DC: GPIO11, CS: GPIO12, BL: GPIO13});
-    display.Initialize(Display::Dir::Rotate0);
-    terminal.SetFont(Font::shinonome16)
-        .AttachDisplay(display).AttachKeyboard(keyboard);
+    USBHost::Initialize();
+    ::i2c_init(i2c0, 400 * 1000);
+    GPIO4.set_function_I2C0_SDA().pull_up();
+    GPIO5.set_function_I2C0_SCL().pull_up();
+    SSD1306 display(i2c0, 0x3c);
+    display.Initialize();
+    terminal.SetFont(Font::shinonome12)
+        .AttachDisplay(display).AttachKeyboard(USBHost::GetKeyboard());
     terminal.Println("ReadLine Test Program");
     for (;;) {
         char* str = terminal.ReadLine(">");
