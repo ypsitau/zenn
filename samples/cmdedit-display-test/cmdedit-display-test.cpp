7597a2d7b38c446e66e4a019f6cc33754c8b31c1
ypsitau <ypsitau@nifty.com>
2025-04-15 16:19:54 +0900

    implementing samples

--- samples/cmdedit-display-test/cmdedit-display-test.cpp
+++ samples/cmdedit-display-test/cmdedit-display-test.cpp
@@ -2,7 +2,9 @@
 #include "pico/stdlib.h"
 #include "jxglib/USBHost.h"
 #include "jxglib/SSD1306.h"
+#include "jxglib/ILI9341.h"
 #include "jxglib/Font/shinonome12.h"
+#include "jxglib/Font/shinonome16.h"
 
 using namespace jxglib;
 
@@ -12,12 +14,20 @@ int main()
 {
     ::stdio_init_all();
     USBHost::Initialize();
+#if 0
     ::i2c_init(i2c0, 400 * 1000);
     GPIO4.set_function_I2C0_SDA().pull_up();
     GPIO5.set_function_I2C0_SCL().pull_up();
     SSD1306 display(i2c0, 0x3c);
+#else
+    // http://www.ze.em-net.ne.jp/~kenken/picogames/index.html
+	::spi_init(spi0, 125 * 1000 * 1000);	// for displays
+	GPIO18.set_function_SPI0_SCK();
+	GPIO19.set_function_SPI0_TX();
+	ILI9341 display(spi0, 240, 320, {RST: GPIO13, DC: GPIO12, CS: GPIO11, BL: GPIO::None});
+#endif
     display.Initialize();
-    terminal.SetFont(Font::shinonome12)
+    terminal.SetFont(Font::shinonome16)
         .AttachDisplay(display).AttachKeyboard(USBHost::GetKeyboard());
     terminal.Println("ReadLine Test Program");
     for (;;) {
