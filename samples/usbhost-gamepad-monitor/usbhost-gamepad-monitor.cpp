8fb1ecc42843f796162f3e7ba95fab79533f4dea
ypsitau <ypsitau@nifty.com>
2025-05-04 15:49:11 +0900

    added usbhost-gamepad-monitor

---
+++ samples/usbhost-gamepad-monitor/usbhost-gamepad-monitor.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "jxglib/ILI9341.h"
+#include "jxglib/ST7789.h"
+#include "jxglib/USBHost.h"
+#include "jxglib/Font/naga10.h"
+
+using namespace jxglib;
+
+int main()
+{
+    ::stdio_init_all();
+    ::spi_init(spi0, 125 * 1000 * 1000);
+    GPIO18.set_function_SPI0_SCK();
+    GPIO19.set_function_SPI0_TX();
+    Display::Terminal terminal;
+    ILI9341 display(spi0, 240, 320, {RST: GPIO13, DC: GPIO12, CS: GPIO11, BL: GPIO::None});
+    //ST7789 display(spi0, 240, 320, {RST: GPIO13, DC: GPIO12, CS: GPIO11, BL: GPIO::None});
+    display.Initialize(Display::Dir::Rotate270);
+    terminal.Initialize().AttachDisplay(display).SetFont(Font::naga10);
+    USBHost::Initialize();
+    USBHost::GamePad gamePad;
+    terminal.Printf("USB GamePad Monitor\n");
+    for (;;) {
+        if (gamePad.HasReportChanged()) {
+            terminal.Printf("%s%s%s%s%s%s%s%s%s%s%s%s%s %X % 1.2f% 1.2f% 1.2f% 1.2f% 1.2f% 1.2f% 1.2f% 1.2f% 1.2f\n",
+                gamePad.Get_Button0()? "0" : ".",
+                gamePad.Get_Button1()? "1" : ".",
+                gamePad.Get_Button2()? "2" : ".",
+                gamePad.Get_Button3()? "3" : ".",
+                gamePad.Get_Button4()? "4" : ".",
+                gamePad.Get_Button5()? "5" : ".",
+                gamePad.Get_Button6()? "6" : ".",
+                gamePad.Get_Button7()? "7" : ".",
+                gamePad.Get_Button8()? "8" : ".",
+                gamePad.Get_Button9()? "9" : ".",
+                gamePad.Get_Button10()? "A" : ".",
+                gamePad.Get_Button11()? "B" : ".",
+                gamePad.Get_Button12()? "C" : ".",
+                gamePad.Get_HatSwitch(),
+                gamePad.Get_Axis0(),
+                gamePad.Get_Axis1(),
+                gamePad.Get_Axis2(),
+                gamePad.Get_Axis3(),
+                gamePad.Get_Axis4(),
+                gamePad.Get_Axis5(),
+                gamePad.Get_Axis6(),
+                gamePad.Get_Axis7(),
+                gamePad.Get_Axis8());
+        }
+        Tickable::Sleep(300);
+    }
+}
