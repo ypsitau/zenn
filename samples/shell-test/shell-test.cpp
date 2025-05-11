f40209aeac14e2c330af2024b75fe01c2b82c0b8
ypsitau <ypsitau@nifty.com>
2025-05-11 12:09:32 +0900

    updated article

--- samples/shell-test/shell-test.cpp
+++ samples/shell-test/shell-test.cpp
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "jxglib/SSD1306.h"
 #include "jxglib/USBHost.h"
+#include "jxglib/Serial.h"
 #include "jxglib/Shell.h"
 #include "jxglib/Font/shinonome12.h"
 
@@ -10,7 +11,7 @@ using namespace jxglib;
 ShellCmd(argtest, "tests command line arguments")
 {
     for (int i = 0; i < argc; i++) {
-        terminal.Printf("argv[%d] \"%s\"\n", i, argv[i]);
+        out.Printf("argv[%d] \"%s\"\n", i, argv[i]);
     }
     return 0;
 }
@@ -19,17 +20,9 @@ int main()
 {
     ::stdio_init_all();
     //-------------------------------------------------------------------------
-    USBHost::Initialize();
-    USBHost::Keyboard keyboard;
-    ::i2c_init(i2c0, 400 * 1000);
-    GPIO4.set_function_I2C0_SDA().pull_up();
-    GPIO5.set_function_I2C0_SCL().pull_up();
-    SSD1306 display(i2c0, 0x3c);
-    Display::Terminal terminal;
-    terminal.Initialize().AttachDisplay(display.Initialize())
-        .AttachKeyboard(keyboard.SetCapsLockAsCtrl()).SetFont(Font::shinonome12);
-    Shell::AttachTerminal(terminal);
-    terminal.Println("Shell with SSD1306");
+    Serial::Terminal terminal;
+    Shell::AttachTerminal(terminal.Initialize());
+    terminal.Println("Shell on Serial Terminal");
     //-------------------------------------------------------------------------
     for (;;) {
         // any jobs
