87e5805488a29ce4c84d47f74bf0b1560ffbecee
ypsitau <ypsitau@nifty.com>
2025-05-25 18:35:23 +0900

    updated sample

--- samples/shell-test/shell-test.cpp
+++ samples/shell-test/shell-test.cpp
@@ -11,22 +11,23 @@ using namespace jxglib;
 int main()
 {
     ::stdio_init_all();
-    //-------------------------------------------------------------------------
-    //Display::Terminal terminal;
-    //USBHost::Initialize();
-    //USBHost::Keyboard keyboard;
-    //::spi_init(spi1, 125 * 1000 * 1000);
-    //GPIO14.set_function_SPI1_SCK();
-    //GPIO15.set_function_SPI1_TX();
-    //ST7789 display(spi1, 240, 320, {RST: GPIO10, DC: GPIO11, CS: GPIO12, BL: GPIO13});
-    //terminal.Initialize().AttachDisplay(display.Initialize(Display::Dir::Rotate90))
-    //    .AttachKeyboard(keyboard.SetCapsLockAsCtrl()).SetFont(Font::shinonome16);
-    ////    .AttachKeyboard(Stdio::GetKeyboard()).SetFont(Font::shinonome16);
-    //Shell::AttachTerminal(terminal);
-    //terminal.Println("Shell on ST7789 TFT LCD");
+#if 1
+    Display::Terminal terminal;
+    USBHost::Initialize();
+    USBHost::Keyboard keyboard;
+    ::spi_init(spi1, 125 * 1000 * 1000);
+    GPIO14.set_function_SPI1_SCK();
+    GPIO15.set_function_SPI1_TX();
+    ST7789 display(spi1, 240, 320, {RST: GPIO10, DC: GPIO11, CS: GPIO12, BL: GPIO13});
+    terminal.Initialize().AttachDisplay(display.Initialize(Display::Dir::Rotate90))
+        .AttachKeyboard(keyboard.SetCapsLockAsCtrl()).SetFont(Font::shinonome16);
+    //    .AttachKeyboard(Stdio::GetKeyboard()).SetFont(Font::shinonome16);
+    Shell::AttachTerminal(terminal);
+    terminal.Println("Shell on ST7789 TFT LCD");
+#else
     Serial::Terminal terminal;
     Shell::AttachTerminal(terminal.Initialize());
-    //-------------------------------------------------------------------------
+#endif
     for (;;) {
         // any jobs
         Tickable::Tick();
