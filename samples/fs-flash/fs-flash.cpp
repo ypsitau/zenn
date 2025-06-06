9ea7cc887b5bb47ca0d4f72d749bd6f3f95720bc
ypsitau <ypsitau@nifty.com>
2025-06-06 15:14:39 +0900

    updated an article

--- samples/fs-flash/fs-flash.cpp
+++ samples/fs-flash/fs-flash.cpp
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "jxglib/LFS/Flash.h"
+#include "jxglib/FAT/Flash.h"
 
 using namespace jxglib;
 
 int main()
 {
     ::stdio_init_all();
-    LFS::Flash drive("Drive", 0x0004'0000);  // 256kB
-    FS::File* pFile = FS::OpenFile("Drive:/test.txt", "r");
-    if (pFile) {
-        char line[256];
-        while (pFile->ReadLine(line, sizeof(line)) > 0) {
-            ::printf("%s", line);      
-        }
-        pFile->Close();
-        delete pFile;
-    }
+    LFS::Flash driveLFS1("LFS1:", 0x1010'0000, 0x0004'0000);  // 256kB
+    LFS::Flash driveLFS2("LFS2:", 0x1014'0000, 0x0004'0000);  // 256kB
+    FAT::Flash driveFAT1("FAT1:", 0x1018'0000, 0x0004'0000);  // 256kB
+    FAT::Flash driveFAT2("FAT2:", 0x101c'0000, 0x0004'0000);  // 256kB
+    // any job
 }
