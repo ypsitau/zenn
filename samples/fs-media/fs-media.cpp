038adb20b70872003f4f9a967cd3d0b675a813c1
ypsitau <ypsitau@nifty.com>
2025-06-08 15:16:28 +0900

    published an article

--- samples/fs-media/fs-media.cpp
+++ samples/fs-media/fs-media.cpp
@@ -1,22 +1,24 @@
 #include "pico/stdlib.h"
-#include "jxglib/FAT/USBMSC.h"
+#include "jxglib/FAT/SDCard.h"
 
 using namespace jxglib;
 
 int main()
 {
     ::stdio_init_all();
-    USBHost::Initialize();
-    FAT::USBMSC drive("Drive:");
+    GPIO2.set_function_SPI0_SCK();
+    GPIO3.set_function_SPI0_TX();
+    GPIO4.set_function_SPI0_RX();
+    FAT::SDCard drive("Drive:", spi0, 10'000'000, {CS: GPIO5});
     bool connectedFlag = false;
     for (;;) {
         if (connectedFlag) {
             if (!drive.CheckMounted()) {
+                ::printf("SD Card disconnected.\n");
                 connectedFlag = false;
-                ::printf("USB storage disconnected.\n");
             }
         } else if (drive.Mount()) {
-            ::printf("USB storage connected.\n");
+            ::printf("SD Card connected.\n");
             connectedFlag = true;
             FS::Dir* pDir = FS::OpenDir("Drive:/");
             if (pDir) {
