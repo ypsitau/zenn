11cf700ad73b883bb8328c4839fde3230ce3ad77
ypsitau <ypsitau@nifty.com>
2025-08-10 12:43:49 +0900

    updated article

--- resource/2025-08-06-labo-pwm/pwm-test-1.sh
+++ resource/2025-08-06-labo-pwm/pwm-test-1.sh
@@ -1,9 +1,9 @@
-la -p 2,3,4,5 enable
-pwm 2,3,4,5 func:pwm disable freq:100 counter:0
-pwm2 duty:.2
-pwm3 duty:.4
-pwm4 duty:.6
-pwm5 duty:.8
-pwm 2,3,4,5 enable
-sleep 2000
-la print
+reset pwm
+pwm 2-5 func:pwm freq:100 counter:0
+la -p 2-5 --samplers:1 enable
+sleep 1000
+pwm2 duty:.2; pwm3 duty:.4; pwm4 duty:.6; pwm5 duty:.8
+pwm 2-5 enable
+sleep 1000
+pwm 2-5 disable
+la print --events:80
--- resource/2025-08-06-labo-pwm/pwm-test-2.sh
+++ resource/2025-08-06-labo-pwm/pwm-test-2.sh
@@ -1,7 +1,9 @@
-la -p 2-5 enable
+reset pwm
+pwm 2-5 func:pwm phase-correct:true freq:100 counter:0
+la -p 2-5 --samplers:4 enable
 sleep 1000
-pwm 2-5 func:pwm disable freq:100 counter:0 phase-correct:true
 pwm2 duty:.2; pwm3 duty:.4; pwm4 duty:.6; pwm5 duty:.8
-pwm 2-9 enable
-sleep 2000
-la print
+pwm 2-5 enable
+sleep 4000
+pwm 2-5 disable
+la print --events:80
